1e1dc1c3ae0e59e2bce0cabe63f7457daa5cbd0e
Kinan Mohrez <kinanmohrez2015@gmail.com>
2023-01-18 11:02:47 +0100

    unit test

--- FIDA-TodoList/src/app/services/todo.service.spec.ts
+++ FIDA-TodoList/src/app/services/todo.service.spec.ts
@@ -1,6 +1,7 @@
 import { async, TestBed } from '@angular/core/testing';
 import { ToastrModule } from 'ngx-toastr';
 import { TodoService } from './todo.service';
+import { Todo } from '../models/todo';
 
 describe('TodoService', () => {
   let service: TodoService;
@@ -10,12 +11,43 @@ describe('TodoService', () => {
       imports: [ToastrModule.forRoot()],
     });
     service = TestBed.inject(TodoService);
+
+    let store = {} as any;
+    const mockLocalStorge = {
+      getItems:(item:string):string => {
+        return item in store ? store[item] : null;
+      },
+      setItems: (item: string, value: string) => {
+      store[item] = `${value}`;
+    }
+    }
+    spyOn(localStorage , 'getItem').and.callFake(mockLocalStorge.getItems);
+    spyOn(localStorage , 'setItem').and.callFake(mockLocalStorge.setItems);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
 
+  describe ('getItem', () => {
+    it('should return stored item from localStorage',() => {
+      localStorage.setItem('items', 'Todo');
+      expect(service.getLocalStorage('items')).toEqual('Todo');
+    });
   });
 
 
-});
+  describe('setItem',  () => {
+    let todoExemple : Todo[] = [{
+      id : 10 ,
+      title : 'titleExample',
+      isCompleted :false ,
+      date : new Date
+    }] ;
+    it ('should store Todo in localStorage', () => {
+      service.setLocalStorage(todoExemple,'keyExample')
+      expect(localStorage.getItem('keyExample')).toContain('titleExample');
+    });
+  })
+
+})
+
+
+
--- FIDA-TodoList/src/app/services/todo.service.ts
+++ FIDA-TodoList/src/app/services/todo.service.ts
@@ -6,45 +6,63 @@ import { ToastrService } from 'ngx-toastr';
   providedIn: 'root'
 })
 export class TodoService {
-  todoList : Todo[]
+  todoList : Todo[];
+  localStorge_KEY: string = 'TodoListLocalStorage';
 
-  constructor(private deletePopup: ToastrService, private AddItemPopup: ToastrService, private compliteItemPopup: ToastrService) {
-    if (JSON.parse(localStorage.getItem('TodoListLocalStorage')!) !== null) {
-    this.todoList  =  JSON.parse(localStorage.getItem('TodoListLocalStorage')!)
+    // set localStorage
+   setLocalStorage (TODOlist : Todo[] , key: string) {
+    localStorage.setItem(key, JSON.stringify(TODOlist));
+   }
+
+
+    // get localStorage
+    getLocalStorage (key : string) {
+      return localStorage.getItem(key);
+    }
+
+  constructor(private Popup: ToastrService) {
+
+
+    if (JSON.parse(this.getLocalStorage(this.localStorge_KEY)!) !== null) {
+    this.todoList  =  JSON.parse(this.getLocalStorage(this.localStorge_KEY)!)
       }
       else
-      { this.todoList =[
-    {
-      id: 1,
-      title: 'Todo One',
-      isCompleted: false,
+      {
+        this.todoList =[
+          {
+            id: 1,
+            title: 'Todo One',
+            isCompleted: false,
 
-      date: new Date('4-15-2022')
-    },
-    {
-      id: 2,
-      title: 'Todo Two',
-      isCompleted: false,
+            date: new Date('4-15-2022')
+          },
+          {
+            id: 2,
+            title: 'Todo Two',
+            isCompleted: false,
 
-      date: new Date('5-15-2022')
-    },
-    {
-      id: 3,
-      title: 'Todo Three',
-      isCompleted: false,
+            date: new Date('5-15-2022')
+          },
+          {
+            id: 3,
+            title: 'Todo Three',
+            isCompleted: false,
 
-      date: new Date('6-15-2022')
-    }
+            date: new Date('6-15-2022')
+          }
   ];
    }}
+
 
 // delete todo
    deleteTodo(item:Todo) {
     let index = this.todoList.indexOf(item);
     this.todoList.splice(index, 1);
-    this.deletePopup.error(`Todo ${item.id} Deleted!`);
-    localStorage.setItem('TodoListLocalStorage', JSON.stringify(this.todoList));
+    this.Popup.error(`Todo ${item.title} Deleted!`);
+
+    this.setLocalStorage(this.todoList ,this.localStorge_KEY);
   }
+
   // Add todo
    addTodo(title:string) {
     let id = this.todoList.length + 2;
@@ -56,15 +74,16 @@ export class TodoService {
       title: title
     }
     this.todoList.unshift(item);
-    this.AddItemPopup.success(` ${item.title} Added!`);
-    localStorage.setItem('TodoListLocalStorage', JSON.stringify(this.todoList));
+    this.Popup.success(` ${item.title} Added!`);
+    this.setLocalStorage(this.todoList ,this.localStorge_KEY);
   }
 
   // complited todo item
   completeTodo (item : Todo) {
     item.isCompleted = true;
     console.log(item.isCompleted)
-    this.compliteItemPopup.info(` ${item.title} Todo succesfully completed!`);
-    localStorage.setItem('TodoListLocalStorage', JSON.stringify(this.todoList));
+    this.Popup.info(` ${item.title} Todo succesfully completed!`);
+
+    this.setLocalStorage(this.todoList , this.localStorge_KEY);
   }
 }
